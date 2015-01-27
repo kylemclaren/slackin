18e21740c42a105724ec2cef520fd1d5db760075
Kyle McLaren <kylemclaren@users.noreply.github.com>
2015-01-27 11:33:53 +0200

    cc

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ require('slackin')({
   token: process.env.SLACK_API_TOKEN, // required
   interval: 1000,
   org: process.env.SLACK_ORG, // required
-  channel: process.env.SLACK_CHANNEL || guest, // for single channel mode,
+  channel: process.env.SLACK_CHANNEL, // for single channel mode,
   silent: false // suppresses warnings
 }).listen(3000);
